f6db56b1b0fc5199b20045bac89e6c593d6b97f0
NDCongSP <ndcong08cddv02@gmail.com>
2026-01-26 22:08:09 +0700

    update

---
+++ sourceCode/GiamSat.API/Scada_TrackingTIme_Revo/Form1.cs
@@ -0,0 +1,25 @@
+﻿using EasyScada.Core;
+using EasyScada.Winforms.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scada_TrackingTIme_Revo
+{
+    public partial class Form1 : Form
+    {
+        private EasyDriverConnector _easyDriverConnector = new EasyDriverConnector();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
