fd522d474f5c5ede5f0bb52774b5ec624d9437e4
NDCongSP <ndcong08cddv02@gmail.com>
2026-03-17 08:26:37 +0700

    update

---
+++ sourceCode/GiamSat.API/GiamSat.Scada/AsyncAutoResetEvent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GiamSat.Scada
+{
+    public class AsyncAutoResetEvent
+    {
+        private static readonly Task _completed = Task.FromResult(true);
+        private readonly Queue<TaskCompletionSource<bool>> _waits = new Queue<TaskCompletionSource<bool>>();
+        private bool _signaled;
+
+        public Task WaitAsync(CancellationToken token = default)
+        {
+            lock (_waits)
+            {
+                if (_signaled)
+                {
+                    _signaled = false;
+                    return _completed;
+                }
+
+                var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+                if (token.CanBeCanceled)
+                    token.Register(() => tcs.TrySetCanceled(token));
+                _waits.Enqueue(tcs);
+                return tcs.Task;
+            }
+        }
+
+        public void Set()
+        {
+            TaskCompletionSource<bool>? toRelease = null;
+            lock (_waits)
+            {
+                if (_waits.Count > 0)
+                    toRelease = _waits.Dequeue();
+                else if (!_signaled)
+                    _signaled = true;
+            }
+            toRelease?.TrySetResult(true);
+        }
+    }
+}
---
+++ sourceCode/GiamSat.API/GiamSat.Scada/AutoRollingTagChangedModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiamSat.Scada
+{
+    public class AutoRollingTagChangedModel
+    {
+        public int RevoId { get; set; } = 0;
+
+        public int Part_Code { get; set; } = 0;
+        public int Recipe_Settings { get; set; } = 0;
+        public int StepRun { get; set; } = 0;
+
+        public int Part_Name1 { get; set; } = 0;
+        public int Part_Name2 { get; set; } = 0;
+        public int Part_Name3 { get; set; } = 0;
+        public int Part_Name4 { get; set; } = 0;
+        public int Part_Name5 { get; set; } = 0;
+        public int Part_Name6 { get; set; } = 0;
+        public int Part_Name7 { get; set; } = 0;
+        public int Part_Name8 { get; set; } = 0;
+    }
+}
