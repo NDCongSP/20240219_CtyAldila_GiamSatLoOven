7febc013b36e7dba709eff13966560a00d40265b
NDCongSP <ndcong08cddv02@gmail.com>
2026-05-20 22:37:53 +0700

    update emtity cho sanding log vaf realtime

---
+++ sourceCode/GiamSat.API/GiamSat.Models/Entities/FT016_SandingLogData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiamSat.Models
+{
+    [Table("FT016")]
+    public class FT016_SandingLogData
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        public string? CreatedMachine { get; set; }
+
+        public string? Part { get; set; }
+
+        public string? Work { get; set; }
+    }
+}
